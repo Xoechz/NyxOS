46148362e01356997c57ffa4db574d9367197ee6
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-07 19:43:24 +0200

    first basic config

---
+++ home.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "elias";
+    home.homeDirectory = "/home/elias";
+
+    home.packages = with pkgs; [
+        neofetch
+        zip
+        xz
+        unzip
+
+        ripgrep
+        fzf
+
+        mtr
+        iperf3
+        dnsutils
+
+        which
+        tree
+
+        nix-output-monitor
+
+        btop
+        iotop
+        iftop
+
+        strace
+        ltrace
+        lsof
+
+        sysstat
+        lm_sensors
+
+        ethtool
+        pciutils
+        usbutils
+    ];
+
+    programs.git = {
+        enable = true;
+        userName = "Elias Leonhardsberger";
+        userEmail = "elias.leonhardsberger@gmail.com";
+    };
+
+    programs.bash = {
+        enable = true;
+        enableCompletion = true;
+    };
+
+    home.stateVersion = "24.05";
+
+    programs.home-manager.enable = true;
+}
