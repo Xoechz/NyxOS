25c12a7650f3b50f9060b61112e1d437c9101283
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-08 21:47:24 +0200

    moved to laptop

--- hosts/EliasLaptop/default.nix
+++ hosts/EliasLaptop/default.nix
@@ -15,8 +15,10 @@
     ];
 
   # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "EliasLaptop"; # Define your hostname.
