43fad5e584399071894ce5eedd0b17e77a41bc2a
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-19 21:01:57 +0200

    fixed steam dependencies

--- modules/steam.nix
+++ modules/steam.nix
@@ -7,4 +7,16 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        openssl_1_1
+      ];
+    };
+  };
 }
