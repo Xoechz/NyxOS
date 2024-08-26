ce5c4fdf878a281915d4d6512f20c793bf78c768
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-26 21:20:33 +0200

    switches to code.fhs to enable debuging

--- home/editors.nix
+++ home/editors.nix
@@ -1,27 +1,10 @@
-# vscode with all extensions for nix development and jetbrains programs. New extensions need be be installed via this config
+# vscode and jetbrains programs.
 { pkgs, ... }:
 
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      # ms-dotnettools.csdevkit
-      # ms-dotnettools.csharp
-      # ms-dotnettools.vscodeintellicode-csharp
-      visualstudioexptteam.vscodeintellicode
-      ms-vscode.cpptools
-      ms-vscode.cmake-tools
-      mhutchie.git-graph
-      github.copilot
-      github.copilot-chat
-      bierner.markdown-mermaid
-      davidanson.vscode-markdownlint
-      arrterian.nix-env-selector
-      jnoortheen.nix-ide
-      emmanuelbeziat.vscode-great-icons
-      tomoki1207.pdf
-      mkhl.direnv
-    ];
+    package = pkgs.vscode.fhs;
   };
 
   # jetbrains programs
