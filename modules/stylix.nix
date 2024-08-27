e2b8546e7a887bb5536ddbef7f1d7336fa8ceebd
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-27 18:33:16 +0200

    stylic cursor and font sizes

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,9 +4,21 @@
   stylix.enable = true;
   stylix.image = ../nixos-wallpaper-catppuccin-mocha.png;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
   stylix.cursor = {
     size = 24;
+    name = "Bibata-Modern-Classic";
+    package = pkgs.bibata-cursors;
   };
+
   stylix.targets.grub.enable = true;
+  stylix.targets.grub.useImage = true;
   stylix.targets.nixos-icons.enable = true;
+
+  stylix.fonts.sizes = {
+    applications = 10;
+    terminal = 10;
+    desktop = 10;
+    popups = 10;
+  };
 }
