4e5faafb060db69ca6e9e997f5f171e11b93cd28
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-08-27 18:00:03 +0200

    enabled stylix for grub

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,4 +4,9 @@
   stylix.enable = true;
   stylix.image = ../nixos-wallpaper-catppuccin-mocha.png;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  stylix.cursor = {
+    size = 24;
+  };
+  stylix.targets.grub.enable = true;
+  stylix.targets.nixos-icons.enable = true;
 }
