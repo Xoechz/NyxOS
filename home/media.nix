4f2d27e6d2c94f534a09753f269521035749a37a
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-11-19 23:09:02 +0100

    added obs

--- home/media.nix
+++ home/media.nix
@@ -54,4 +54,13 @@
       theme = spicePkgs.themes.catppuccin;
       colorScheme = "mocha";
     };
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
