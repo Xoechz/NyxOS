bf6708080244c05553497c43d8d736b96d61d32b
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2026-02-20 09:45:32 +0100

    removed hacky plasma override and enabled plasma-login-manager

--- modules/kde.nix
+++ modules/kde.nix
@@ -24,9 +24,8 @@
     # set KDE to wayland
     services.displayManager = {
       defaultSession = "plasma";
-      sddm = {
+      plasma-login-manager = {
         enable = true;
-        wayland.enable = true;
       };
     };
 
@@ -39,56 +38,6 @@
     ];
 
     environment.variables.NIXOS_OZONE_WL = "1";
-
-    nixpkgs.overlays = lib.singleton (final: prev: {
-      kdePackages = prev.kdePackages.overrideScope (
-        kdeFinal: kdePrev: {
-          # https://old.reddit.com/r/NixOS/comments/1pdtc3v/kde_plasma_is_slow_compared_to_any_other_distro/
-          # https://github.com/NixOS/nixpkgs/issues/126590#issuecomment-3194531220
-          plasma-workspace =
-            let
-              # the package we want to override
-              basePkg = kdePrev.plasma-workspace;
-              # a helper package that merges all the XDG_DATA_DIRS into a single directory
-              xdgdataPkg = final.stdenv.mkDerivation {
-                name = "${basePkg.name}-xdgdata";
-                buildInputs = [ basePkg ];
-                dontUnpack = true;
-                dontFixup = true;
-                dontWrapQtApps = true;
-                installPhase = ''
-                  mkdir -p $out/share
-                  ( IFS=:
-                    for DIR in $XDG_DATA_DIRS; do
-                      if [[ -d "$DIR" ]]; then
-                        ${prev.lib.getExe prev.lndir} -silent "$DIR" $out
-                      fi
-                    done
-                  )
-                '';
-              };
-              # undo the XDG_DATA_DIRS injection that is usually done in the qt wrapper
-              # script and instead inject the path of the above helper package
-              derivedPkg = basePkg.overrideAttrs {
-                preFixup = ''
-                  for index in "''${!qtWrapperArgs[@]}"; do
-                    if [[ ''${qtWrapperArgs[$((index+0))]} == "--prefix" ]] && [[ ''${qtWrapperArgs[$((index+1))]} == "XDG_DATA_DIRS" ]]; then
-                      unset -v "qtWrapperArgs[$((index+0))]"
-                      unset -v "qtWrapperArgs[$((index+1))]"
-                      unset -v "qtWrapperArgs[$((index+2))]"
-                      unset -v "qtWrapperArgs[$((index+3))]"
-                    fi
-                  done
-                  qtWrapperArgs=("''${qtWrapperArgs[@]}")
-                  qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
-                  qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
-                '';
-              };
-            in
-            derivedPkg;
-        }
-      );
-    });
   };
 
   # Home Module plasma-manager: configure plasma manager
@@ -611,7 +560,7 @@
             path = "${config.home.homeDirectory}/NyxOS/images/heightlines_v3";
             interval = 10;
           };
-        colorScheme = "CatppuccinMochaFlamingo";
+        colorScheme = "CatppuccinMochaPeach";
         soundTheme = "ocean";
         splashScreen = {
           theme = "None";
